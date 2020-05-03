864e73199a7f0cc9d8b8b98d4edceee04562ee0c
myatsuphyo <myatsuphyo9@gmail.com>
2020-05-03 13:54:32 +0630

    Fix passport bug

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -1,9 +1,11 @@
 <?php
 
 namespace App\Providers;
+use Laravel\Passport\Passport;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+
 
 class AuthServiceProvider extends ServiceProvider
 {
